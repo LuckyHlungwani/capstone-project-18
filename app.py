aea7b9c62fefed9f3ff70ad0c6f4cb7c575c00ec
Loudrick <hlungwaniloudrick96@gmail.com>
2024-10-22 23:10:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -591,4 +591,5 @@ def get_unread_notifications_count(user_id):
     return count
 
 if __name__ == "__main__":
-       app.run(debug=True, port=port) 
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(debug=True, port=port, host='0.0.0.0')  
